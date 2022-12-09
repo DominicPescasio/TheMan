21e98b911ec5f1993b77bd871a1fa4721ad9a433
Kyan0ver9000 <115186321+Kyan0ver9000@users.noreply.github.com>
2022-12-09 13:01:31 -0800

    made the power system work finally

--- The One And Only MAN/Assets/K/powerUps/PowerUpBlock.cs
+++ The One And Only MAN/Assets/K/powerUps/PowerUpBlock.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class PowerUpBlock : MonoBehaviour
 {
+   
+   
     [SerializeField]
     private Sprite _inactiveSprite;
 
     [SerializeField]
     private GameObject _powerUp;
+
+    public int block = 1;
 
     private bool _used;
     private void OnCollisionEnter2D(Collision2D other)
@@ -17,6 +21,9 @@ public class PowerUpBlock : MonoBehaviour
         {
             GetComponent<SpriteRenderer>().sprite = _inactiveSprite;
             Instantiate(_powerUp, transform.position, Quaternion.identity);
+            _used = true;
         }
+        
     }
+
 }
