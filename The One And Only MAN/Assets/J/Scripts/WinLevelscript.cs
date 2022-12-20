e67185ad620f7f8022a315cf549397bc830eb49d
Kyan0ver9000 <115186321+Kyan0ver9000@users.noreply.github.com>
2022-12-20 13:07:48 -0800

    i finished level one

--- The One And Only MAN/Assets/J/Scripts/WinLevelscript.cs
+++ The One And Only MAN/Assets/J/Scripts/WinLevelscript.cs
@@ -20,7 +20,7 @@ public class WinLevelscript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (sceneIndex == 3)
+        if (sceneIndex == 5)
         {
             SceneManager.LoadScene("WinScreen");
         }
